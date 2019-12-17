b359cbb7b6717c17f0565cfb93efde5b9075e365
mrib5 <ibutura13@yahoo.com>
2019-12-17 20:40:10 +0000

    Finished program

--- README.md
+++ README.md
@@ -2,19 +2,6 @@
 Find out your typing speed, how many words and characters per minute.
 
 Python 3.7.3 was used, with the following libraries;
-random
+random, time, re
 
 The english2.txt file was downloaded from http://www.gwicks.net/dictionaries.htm
-
-
-
-### Initial Projet Plan
-Generate 50 random numbers from 0 to 65197(amount of lines(one word per line) in english2.txt)
-Get the words associated to that line number.
-Add them to a list.
-Print the words out.
-Start timer.
-Let user type out all the words.
-Add every word the user types in, into another list.
-Stop timer.
-Find out how many words were typed per minute.
--- typingspeed.py
+++ typingspeed.py
@@ -1,8 +1,32 @@
 from random import randint
+import time
+import re
 
+f = open('english2.txt')
+lines = f.readlines()
 wordsToType = 50
+randomNumbers = []
 listOfWordsToType = []
 
-f = open('english2.txt', 'r')
-for word in f:
-    print(word[20])
+def speedTestSetup():
+    for j in range(wordsToType):
+        number = randint(0,65198)
+        randomNumbers.append(number)
+    for i in randomNumbers:
+        listOfWordsToType.append(lines[i][:-1])
+    #print(' '.join(listOfWordsToType))
+def typingSpeedTest():
+    print("[*] Words required to be re-written are displayed below:\n")
+    print(' '.join(listOfWordsToType))
+    print("\n")
+    ready = input("[*] When you are ready to start please press Enter.")
+    start = time.time()
+    userWords = input("[*] Type in the words you see above:\n")
+    end = time.time()
+    wordsInputted = re.split(r'[^0-9A-Za-z]+',userWords)
+    timeTakenS = (end-start)
+    timeTakenM = timeTakenS/60
+    wordsPerMinute = len(wordsInputted)/timeTakenM
+    print("[*] That is roughly "+str(wordsPerMinute)+" words per minute")
+speedTestSetup()
+typingSpeedTest()
